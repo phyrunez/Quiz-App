d2eb7c5bfc3d63ad10332db0b896635393c4c1c4
phyrunez <ebukajudee302@gmail.com>
2023-04-30 01:35:33 +0100

    modified script

--- public/scripts/quizzer.js
+++ public/scripts/quizzer.js
@@ -41,7 +41,7 @@ class Quizzer {
                                 }
                         })
                         .catch(err => {
-                                        reject({});
+                                reject({});
                         })
                 })
         }
